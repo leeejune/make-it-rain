f786ebcea78bb9b9e5666fbbd1708e95e8d040a7
matthewputra <putra@uw.edu>
2019-11-13 18:39:31 -0800

    Added 2 data sets

---
+++ analysis.R
@@ -0,0 +1,11 @@
+# This file is used to calculate values and function that will be use in the application
+
+# Import packages
+
+# Store data sources into data frame
+# Data frame of the starting salary by college
+sal_by_col_df <- read.csv("./data/salaries-by-college-type.csv", stringsAsFactors = FALSE)
+# Data frame of the starting salary by state
+sal_by_reg_df <- read.csv("./data/salaries-by-region.csv", stringsAsFactors = FALSE)
+
+
